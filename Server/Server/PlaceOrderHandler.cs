410533fac00c77af55f794889c679576c2749c77
anurag003iitt <anurag003iitt@gmail.com>
2016-06-24 16:57:53 +0530

    Add files via upload

---
+++ Server/Server/PlaceOrderHandler.cs
@@ -0,0 +1,35 @@
+﻿using NServiceBus;
+using NServiceBus.Logging;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
+    {
+        static ILog log = LogManager.GetLogger<PlaceOrderHandler>();
+        IBus bus;
+
+        public PlaceOrderHandler(IBus bus)
+        {
+            this.bus = bus;
+        }
+
+        public void Handle(PlaceOrder message)
+        {
+            log.InfoFormat("Order for Product:{0} placed with id: {1}", message.Product, message.Id);
+
+            log.InfoFormat("Publishing: OrderPlaced for Order Id: {0}", message.Id);
+
+            var orderPlaced = new OrderPlaced
+            {
+                OrderId = message.Id
+            };
+            bus.Publish(orderPlaced);
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,28 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            var busConfiguration = new BusConfiguration();
+            busConfiguration.EndpointName("Server");
+            busConfiguration.UseSerialization<JsonSerializer>();
+            busConfiguration.EnableInstallers();
+            busConfiguration.UsePersistence<InMemoryPersistence>();
+
+            using (var bus = Bus.Create(busConfiguration).Start())
+            {
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+        }
+    }
+}
