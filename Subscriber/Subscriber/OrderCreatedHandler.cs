bee4376a1c4f3469897cc8c5b62233cf63d449ae
anurag003iitt <anurag003iitt@gmail.com>
2016-06-24 17:03:23 +0530

    Add files via upload

---
+++ Subscriber/Subscriber/OrderCreatedHandler.cs
@@ -0,0 +1,21 @@
+﻿using NServiceBus;
+using NServiceBus.Logging;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subscriber
+{
+    public class OrderCreatedHandler : IHandleMessages<OrderPlaced>
+    {
+        static ILog log = LogManager.GetLogger<OrderCreatedHandler>();
+
+        public void Handle(OrderPlaced message)
+        {
+            log.InfoFormat("Handling: OrderPlaced for Order Id: {0}", message.OrderId);
+        }
+    }
+}
